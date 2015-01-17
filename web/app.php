7edd33d8943d8f31eb176aa57c8a24d54f212541
Wardany <swardany@tabssoft.com>
2015-01-17 16:07:22 +0200

    Realestate first commit

--- web/app.php
+++ web/app.php
@@ -8,15 +8,15 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+use Symfony\Component\HttpFoundation\Request;
 require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__ . '/../app/AppKernel.php';
 
 
-$request = Sonata\PageBundle\Request\RequestFactory::createFromGlobals('host_with_path');
+//$request = Sonata\PageBundle\Request\RequestFactory::createFromGlobals('host_with_path');
 
 $kernel = new AppKernel('prod', false);
-
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 

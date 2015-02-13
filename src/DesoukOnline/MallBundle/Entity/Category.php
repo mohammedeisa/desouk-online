35dd040777dab6d56f916d677554aec4c9433b99
Wardany <swardany@tabssoft.com>
2015-02-13 15:03:56 +0200

    Mall Category

--- src/DesoukOnline/MallBundle/Entity/Category.php
+++ src/DesoukOnline/MallBundle/Entity/Category.php
@@ -4,6 +4,8 @@ namespace DesoukOnline\MallBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * News
@@ -54,13 +56,125 @@ class Category
      */
     protected $vendors;
 
-    /**
+    ////////////////////////////////  Image Field ////////////////////////////////////////
+	/**
+     * Image path
+     *
      * @var string
      *
-     * @ORM\ManyToOne(targetEntity="Application\Sonata\MediaBundle\Entity\Media" , cascade={"remove"})
-     * @ORM\JoinColumn(name="image_id", referencedColumnName="id", onDelete="SET NULL")
+     * @ORM\Column(type="text", length=255, nullable=false)
      */
-    private $image;
+    protected $path;
+	/**
+     * Image file
+     *
+     * @var File
+     *
+     * @Assert\File(
+     *     maxSize = "512k",
+     *     mimeTypes = {"image/jpeg", "image/gif", "image/png", "image/tiff"},
+     *     maxSizeMessage = "The maxmimum allowed file size is 512KB.",
+     *     mimeTypesMessage = "Only the filetypes image are allowed."
+     * )
+     */
+    protected $file;
+	
+	/**
+	 * Sets file.
+	 *
+	 * @param UploadedFile $file
+	*/
+	public function setFile(UploadedFile $file = null)
+	{
+	    $this->file = $file;
+	}
+	
+	/**
+	 * Sets path.
+	 *
+	*/
+	public function setPath($path = null)
+	{
+	    $this->path = $path;
+	}
+	
+	/**
+	 * Get file.
+	 *
+	 * @return UploadedFile
+	*/
+	public function getFile()
+	{
+	    return $this->file;
+	}
+  	
+	public function getAbsolutePath()
+	{
+		return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+	}
+	
+	public function getWebPath()
+	{
+		return null === $this->path ? null : '/'.$this->getUploadDir().'/'.$this->path;
+	}
+	
+	protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+	
+	protected function getUploadDir()
+	{
+		// get rid of the __DIR__ so it doesn't screw when displaying uploaded doc/image in the view.
+	    return 'uploads/mall_category';
+	}
+	
+	
+	public function upload()
+	{
+	    // the file property can be empty if the field is not required
+	    if (null === $this->getFile()) {
+	        return;
+	    }
+		$file = $this->getAbsolutePath();
+		if (file_exists($file)) {
+	        unlink($file); 
+	    }
+		
+		$filename = sha1(uniqid(mt_rand(), true));
+        $this->path = $filename.'.'.$this->file->guessExtension();
+		
+	
+	    // we use the original file name here but you should
+	    // sanitize it at least to avoid any security issues
+	
+	    // move takes the target directory and target filename as params
+	    $this->getFile()->move(
+	        $this->getUploadRootDir(),
+	        $this->path
+	    );
+	
+	    // set the path property to the filename where you've saved the file
+	    //$this->filename = $this->getFile()->getClientOriginalName();
+	
+	    // clean up the file property as you won't need it anymore
+	    $this->setFile(null);
+	}
+	
+	/**
+	 * Called before entity removal
+	 *
+	 * @ORM\PreRemove()
+	 */
+	public function removeUpload()
+	{
+	    if ($file = $this->getAbsolutePath()) {
+	        unlink($file); 
+	    }
+	}
+	////////////////////////////////////////////////////////////////////////////////////////
 
     /**
      * @var \DateTime
@@ -181,21 +295,6 @@ class Category
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    /**
-     * @param string $image
-     */
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
 
 
     /**

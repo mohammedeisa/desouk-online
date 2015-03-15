63b33e0636b6694bada0c14526f8dec7b0a8e0ca
Mohammed <mohammed.ezz.2011@gmail.com>
2015-03-15 17:20:19 +0200

    final web development and media upload

--- src/DesoukOnline/WebDevelopmentBundle/Entity/Portfolio.php
+++ src/DesoukOnline/WebDevelopmentBundle/Entity/Portfolio.php
@@ -32,7 +32,7 @@ class Portfolio
     /**
      * @var string
      *
-     * @ORM\Column(name="description", type="string", length=255)
+     * @ORM\Column(name="description", type="text")
      */
     private $description;
 
